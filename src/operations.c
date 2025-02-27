675e9cb30ea02d6565d148fb5a6fffd80613a2de
LilianBernot <lilian.bernot@gmail.com>
2025-02-27 15:34:58 +0100

    refactor(utils-operations): delete commented code

--- src/operations.c
+++ src/operations.c
@@ -8,15 +8,3 @@ double dist(float *U, float *V, int n) {
     }
     return sum;
 }
-
-// Exemple d'utilisation
-// int main() {
-//     float U[] = {1.0, 2.0, 3.0};
-//     float V[] = {4.0, 5.0, 6.0};
-//     int n = 3;
-
-//     double result = dist(U, V, n);
-//     printf("Distance: %lf\n", result);
-
-//     return 0;
-// }
